2b093d16cd5c9973a906b34b6e07eb3a5866f46a
Carlos Cordova <carloscordova9003@gmail.com>
2020-05-13 18:59:51 -0500

    index no contend foud comentado

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 												} else {
 
 													// If no content, include the "No posts found" template.
-													get_template_part( 'template-parts/content/content', 'none' );
+													//get_template_part( 'template-parts/content/content', 'none' );
 
 												}
 												?>
