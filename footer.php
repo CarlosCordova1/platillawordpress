d02a5cfd9b690badf6f1b2b92e7458731d30d9c1
Carlos Cordova <carloscordova9003@gmail.com>
2020-05-13 13:53:35 -0500

    update screenshot, scritp analityc and contacto

--- footer.php
+++ footer.php
@@ -3,12 +3,9 @@
 									<header class="major">
 										<h2>contacto</h2>
 									</header>
-									<p>Ingeniero En sistemas compuntaciones con Maestria en Tecnologias de informacion y comunicacion</p>
+									<p>Ingeniero En sistemas compuntaciones con Maestría en Tecnologías de informaci'on y comunicación</p>
 									<ul class="contact">
-										<li class="icon solid fa-envelope"><a href="#">carloscordova9003@gmail.com</a></li>
-									<!--	<li class="icon solid fa-phone">(000) 000-0000</li>
-										<li class="icon solid fa-home">1234 Somewhere Road #8254<br />
-										Nashville, TN 00000-0000</li>-->
+										<li class="icon solid fa-envelope"><a href="#">contacto@carloscordova.com</a></li>
 									</ul>
 								</section>
 
--- header.php
+++ header.php
@@ -9,4 +9,13 @@
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/main.css" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
  
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166484475-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-166484475-1');
+</script>
 	</head>
